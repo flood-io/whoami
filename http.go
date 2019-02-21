e793f8f9b4e22aa0f3d78514f496717189c9efb0
Tim Koopmans <tim.koops@gmail.com>
2019-02-21 10:50:17 +0100

    add some beef

--- http.go
+++ http.go
@@ -1,26 +1,32 @@
 package main
 
 import (
-  "os"
-  "fmt"
-  "net/http"
-  "log"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
 )
 
 func main() {
-    port := os.Getenv("PORT")
-    if port == "" {
-        port = "8080"
-    }
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 
-    fmt.Fprintf(os.Stdout, "Listening on :%s\n", port)
-    hostname, _ := os.Hostname()
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(os.Stdout, "I'm %s\n", hostname)
- 	fmt.Fprintf(w, "I'm %s\n", hostname)
-    })
+	var builder strings.Builder
+	builder.Grow(1048577)
+	for i := 0; i < 1048577; i++ {
+		builder.WriteByte(0)
+	}
+	beefcake := builder.String()
 
+	fmt.Fprintf(os.Stdout, "Listening on :%s\n", port)
+	hostname, _ := os.Hostname()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(os.Stdout, "I'm %s\n", hostname)
+		fmt.Fprintf(w, "I'm %s\n beefcake:%s", hostname, beefcake)
+	})
 
-    log.Fatal(http.ListenAndServe(":" + port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
-
