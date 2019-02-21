a8f39364e6cd5e3d3e977fdc4dca39a94172c3bb
Tim Koopmans <tim.koops@gmail.com>
2019-02-21 11:23:37 +0100

    add more beef with a random string

--- http.go
+++ http.go
@@ -3,10 +3,25 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
-	"strings"
+	"time"
 )
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func beefcake(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -14,18 +29,11 @@ func main() {
 		port = "8080"
 	}
 
-	var builder strings.Builder
-	builder.Grow(1048577)
-	for i := 0; i < 1048577; i++ {
-		builder.WriteByte(0)
-	}
-	beefcake := builder.String()
-
 	fmt.Fprintf(os.Stdout, "Listening on :%s\n", port)
 	hostname, _ := os.Hostname()
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(os.Stdout, "I'm %s\n", hostname)
-		fmt.Fprintf(w, "I'm %s\n beefcake:%s", hostname, beefcake)
+		fmt.Fprintf(w, "I'm %s\n beefcake:%s", hostname, beefcake(1000000))
 	})
 
 	log.Fatal(http.ListenAndServe(":"+port, nil))
